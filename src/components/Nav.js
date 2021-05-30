5314d5dc5b1b1073d63c11c83dbc1ce68ac94c6e
Sneha <snhthakare@gmail.com>
2021-05-30 18:44:28 +0530

    Adds login and sign up pop up

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import './nav.css'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-function Nav() {
+export default function Nav() {
     return (
         <header className="nav">
             <div className="nav-left-link">
@@ -19,12 +26,107 @@ function Nav() {
                         <li>Host Tools</li>
                     </ul>
                 </div>
-                <a href="/">Trips</a>
-                <a href="/">Messages</a>
+                <FormDialogLogin />
+                <FormDialogSignUp />
+                
                 <PersonOutlineIcon/>
             </div>
         </header>
     )
 }
 
-export default Nav
+function FormDialogLogin() {
+    const [open, setOpen] = React.useState(false);
+  
+    const handleClickOpen = (e) => {
+      e.preventDefault()
+      setOpen(true);
+    };
+  
+    const handleClose = (e) => {
+      e.preventDefault()
+      setOpen(false);
+    };
+  
+    return (
+      <div>
+        <button onClick={handleClickOpen} className="nav-btn">
+          <strong>Log in</strong>
+        </button>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <button onClick={handleClose} className="nav-cross">X</button>
+          <div className="login-form">
+            <h2 className="center">Welcome Back</h2>
+            <span>Email</span>
+            <input type="email" placeholder="Email" />
+            <span>Password</span>
+            <input type="password" placeholder="Password" />
+          </div>
+          <DialogActions className="button-stack">
+            <button onClick={handleClose} className="btn-long">
+              Login
+            </button>
+            <h5>or</h5>
+            <button onClick={handleClose} className="btn-long-transparent">
+                Continue with Apple
+            </button>
+            <button onClick={handleClose} className="btn-long-transparent">
+                Continue with Google
+            </button>
+            <button onClick={handleClose} className="btn-long-transparent">
+                Continue with Facebook
+            </button>
+            <p>Don't have an account?&nbsp;
+                <a href="/">Sign up</a>
+            </p>
+            
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+}
+
+function FormDialogSignUp() {
+    const [open, setOpen] = React.useState(false);
+  
+    const handleClickOpen = (e) => {
+      e.preventDefault()
+      setOpen(true);
+    };
+  
+    const handleClose = (e) => {
+      e.preventDefault()
+      setOpen(false);
+    };
+  
+    return (
+      <div>
+        <button className="nav-btn" onClick={handleClickOpen}>
+          <strong>Sign Up</strong>
+        </button>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <button onClick={handleClose} className="nav-cross">X</button>
+          <div className="login-form"></div>
+          <DialogActions className="button-stack">
+            <h2>Welcome to Turo</h2>
+            <button onClick={handleClose} className="btn-long-transparent">
+                Continue with Apple
+            </button>
+            <button onClick={handleClose} className="btn-long-transparent">
+                Continue with Google
+            </button>
+            <button onClick={handleClose} className="btn-long-transparent">
+                Continue with Facebook
+            </button>
+            <button onClick={handleClose} className="btn-long-transparent">
+                Continue with Email
+            </button>
+            <p>Already have an account?&nbsp;
+                <a href="/">Log in</a>
+            </p>
+            
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+}
